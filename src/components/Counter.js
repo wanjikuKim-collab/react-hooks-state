6bf3f9d668364f79a4c91daf4539c0b405fb60f0
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-11 16:15:33 +0300

    Completed lab

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,15 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    console.log(`before setState: ${count}`);
+
+    setCount(currentCount=> currentCount + 1);
+    setCount(currentCount=> currentCount + 1);
+
+    console.log(`after setState: ${count}`);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>{count}</button>;
 }
 
 export default Counter;
